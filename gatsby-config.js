8052333d744863c245f771e4b653ff2b94313443
Farass12 <farasadiilahareta@gmail.com>
2025-07-29 13:26:02 +0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,152 @@
-// const urljoin = require("url-join")
-// import urlJoin from 'url-join';
-const siteConfig = require("./siteConfig")
-// import siteConfig from "./siteConfig"
+
+const siteConfig = require("./siteConfig");
+
+// module.exports = {
+//   siteMetadata: {
+//     title: siteConfig.name,
+//     author: siteConfig.author,
+//     description: siteConfig.description,
+//     image: siteConfig.image,
+//     siteUrl: "https://stifintangerang.netlify.app/",
+//     social: {
+//       twitter: siteConfig.twitter,
+//       facebook: siteConfig.facebook,
+//       github: siteConfig.github,
+//     },
+//   },
+//   plugins: [
+//     // === Image uploads support ===
+//     {
+//       resolve: "gatsby-source-filesystem",
+//       options: {
+//         path: `${__dirname}/static/img`,
+//         name: "uploads",
+//       },
+//     },
+//     // === Pages content ===
+//     {
+//       resolve: "gatsby-source-filesystem",
+//       options: {
+//         path: `${__dirname}/src`,
+//         name: "pages",
+//       },
+//     },
+
+//     // === Image transformers ===
+//     `gatsby-transformer-sharp`,
+//     `gatsby-plugin-sharp`,
+//     `gatsby-plugin-image`,
+
+//     // === Markdown support with images ===
+//     {
+//       resolve: `gatsby-transformer-remark`,
+//       options: {
+//         plugins: [
+//           {
+//             resolve: "gatsby-remark-relative-images",
+//             options: {
+//               name: "uploads",
+//             },
+//           },
+//           {
+//             resolve: "gatsby-remark-copy-linked-files",
+//             options: {
+//               ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`, `webp`],
+//             },
+//           },
+//           {
+//             resolve: `gatsby-remark-images`,
+//             options: {
+//               maxWidth: 1360,
+//               withWebp: true,
+//               showCaptions: false,
+//               quality: 75,
+//               wrapperStyle: `margin: 7vw 0;`,
+//             },
+//           },
+//           {
+//             resolve: `gatsby-remark-responsive-iframe`,
+//             options: {
+//               wrapperStyle: `margin-bottom: 1.0725rem`,
+//             },
+//           },
+//           `gatsby-remark-prismjs`,
+//           `gatsby-remark-smartypants`,
+//         ],
+//       },
+//     },
+
+//     // === CMS ===
+//     {
+//       resolve: "gatsby-plugin-netlify-cms",
+//       options: {
+//         modulePath: `${__dirname}/src/cms/cms.js`,
+//       },
+//     },
+
+//     // === PostCSS ===
+//     {
+//       resolve: `gatsby-plugin-postcss`,
+//       options: {
+//         postCssPlugins: [
+//           require("postcss-easy-import")(),
+//           require("postcss-custom-properties")({ preserve: false }),
+//           require("postcss-color-function")(),
+//           require("autoprefixer")({ overrideBrowserslist: ["last 1 version"] }),
+//         ],
+//       },
+//     },
+
+//     // === Purge unused CSS ===
+//     {
+//       resolve: `gatsby-plugin-purgecss`,
+//       options: {
+//         printRejected: true,
+//       },
+//     },
+
+//     // === Web App Manifest ===
+//     {
+//       resolve: `gatsby-plugin-manifest`,
+//       options: {
+//         name: siteConfig.name,
+//         short_name: siteConfig.shortName,
+//         start_url: siteConfig.prefix,
+//         background_color: `#ffffff`,
+//         theme_color: `#ffffff`,
+//         display: `standalone`,
+//         icon: siteConfig.manifest_icon,
+//       },
+//     },
+
+//     // === Netlify optimizations ===
+//     `gatsby-plugin-netlify`,
+//     `gatsby-plugin-offline`,
+
+//     // === SEO ===
+//     `gatsby-plugin-react-helmet`,
+//     `gatsby-plugin-sitemap`,
+//   ],
+// };
 
 module.exports = {
   siteMetadata: {
-    title: siteConfig.name,
-    author: siteConfig.author,
-    description: siteConfig.description,
-    image: siteConfig.image,
-    siteUrl: "https://clay-gatsby.netlify.app/",
+    title: "Stifin Tangerang",
+    shortName: "Stifin",
+    author: "Tim Stifin Tangerang",
+    description: "Situs resmi STIFIn Tangerang untuk informasi, edukasi, dan layanan STIFIn.",
+    image: "/img/default-og-image.jpg",
+    siteUrl: "https://stifintangerang.netlify.app/",
+    
+    
     social: {
-      twitter: siteConfig.twitter,
-      facebook: siteConfig.facebook,
-      github: siteConfig.github,
+      twitter: "https://twitter.com/stifintgrng",
+      facebook: "https://facebook.com/stifintangerang",
+      github: "https://github.com/stifintangerang",
     },
   },
   plugins: [
-
     {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
@@ -29,24 +156,10 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src`,
+        path: `${__dirname}/src/pages`,
         name: `pages`,
       },
     },
-    // {
-    //   resolve: "gatsby-source-filesystem",
-    //   options: {
-    //     path: `${__dirname}/static/img`,
-    //     name: "uploads",
-    //   },
-    // },
-    // {
-    //   resolve: "gatsby-source-filesystem",
-    //   options: {
-    //     path: `${__dirname}/src/img`,
-    //     name: "images",
-    //   },
-    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
@@ -63,9 +176,7 @@ module.exports = {
           {
             resolve: "gatsby-remark-copy-linked-files",
             options: {
-              // destinationDir: "public",
-              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff` ,`webp`],
-          
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`, `webp`],
             },
           },
           {
@@ -78,20 +189,11 @@ module.exports = {
               wrapperStyle: `margin: 7vw 0;`,
             },
           },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
           `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
-
         ],
       },
     },
-
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
@@ -112,38 +214,24 @@ module.exports = {
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        printRejected: true,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
-    // `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: siteConfig.name,
-        short_name: siteConfig.shortName,
-        start_url: siteConfig.prefix,
+        name: "Stifin Tangerang",
+        short_name: "Stifin",
+        start_url: "/",
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `standalone`,
-        resolve: `gatsby-plugin-manifest`,
-        icon: siteConfig.manifest_icon,
+        icon: "src/img/icon.png",
       },
     },
     `gatsby-plugin-netlify`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
   ],
-}
-
+}; 
